91424d26e24e3501f9f80188937aadd4930f09f2
NexoNeko <meresidamente@hotmail.com>
2023-05-21 17:02:06 -0300

    rule

---
+++ module3_task0/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update && apt-get install -y git make curl build-essential
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /root/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /root/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install hugo
---
+++ module3_task5/content/posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome to Awesome Inc."
+date: 2022-12-29T14:57:12+01:00
+draft: true
+---
+
