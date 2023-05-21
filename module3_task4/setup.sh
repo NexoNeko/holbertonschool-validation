8f57c9ece69a01c75a0b0e4bbb257f8ac9894593
NexoNeko <meresidamente@hotmail.com>
2023-05-21 17:04:08 -0300

    cleanup

--- module3_task4/setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -y shellcheck snapd git make wget build-essential && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb && snap install mdl
